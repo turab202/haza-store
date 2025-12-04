c5b7ecb4290f39d1c80afa475b7a94b34039b776
turab202 <turabturqb@gmail.com>
2025-12-04 12:51:27 +0300

    light and dark theme

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/theme/custom_themes/text_theme.dart';
+import 'custom_themes/elevated_button_theme.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+  );
+}
